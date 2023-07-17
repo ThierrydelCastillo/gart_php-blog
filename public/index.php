27e27e0b0b50e4424442e54da40b8490cb7163f6
ThierrydelCastillo <dev@del-castillo.fr>
2023-07-17 15:58:26 +0200

    02 - Le routeur

--- public/index.php
+++ public/index.php
@@ -1,23 +1,14 @@
 <?php
 require '../vendor/autoload.php';
-define('VIEW_PATH', dirname(__DIR__) . '/views');
-    require VIEW_PATH . '/layouts/header.php';
 
-$router = new AltoRouter();
+define('DEBUG_TIME', microtime(true));
 
-$router->map('GET', '/blog', function () {
-    require VIEW_PATH .'/post/index.php';
-});
-$router->map('GET', '/blog/category', function () {
-    require VIEW_PATH . '/category/show.php';
-});
+$whoops = new \Whoops\Run;
+$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+$whoops->register();
 
-$match = $router->match();
-
-if (is_array($match)) {
-    $match['target']();
-} else {
-    echo 'ERROR 404';
-}
-
-require VIEW_PATH . '/layouts/footer.php';
+$router = new App\Router(dirname(__DIR__) . '/views');
+$router
+    ->get('/blog', 'post/index', 'blog')
+    ->get('/blog/category', 'category/show', 'category')
+    ->run();
--- views/layouts/default.php
+++ views/layouts/default.php
@@ -1,15 +1,25 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="fr" class="h-100">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.2.1/dist/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-    <title>Document</title>
+    <title><?= $title ?? 'Mon site' ?></title>
 </head>
-<body>
+<body class="d-flex flex-column h-100">
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
         <a href="#" class="navbar-brand">Mon site</a>
     </nav>
     <div class="container mt-4">
+        <?= $content ?>
+    </div>
 
-
+    <footer class="bg-light py-4 footer mt-auto">
+        <div class="container">
+            <?php if(defined('DEBUG_TIME')): ?>
+                Page générée en <?= round(1000 * (microtime(true) - DEBUG_TIME)) ?>ms
+            <?php endif ?>
+        </div>
+    </footer>
+</body>
+</html>
