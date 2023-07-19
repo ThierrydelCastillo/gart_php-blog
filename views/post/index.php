4d37882701db34d2e5d9e11e29590f2afdc8bb49
ThierrydelCastillo <dev@del-castillo.fr>
2023-07-19 14:00:34 +0200

    05 - Pagination

--- views/post/index.php
+++ views/post/index.php
@@ -1,12 +1,35 @@
 <?php
-use App\Helpers\Text;
 use App\Model\Post;
+use App\Helpers\Text;
 
 $title = 'Mon blog';
 $pdo = new PDO('mysql:dbname=tutoblog;host=127.0.0.1', 'root', 'root', [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
 ]);
-$query = $pdo->query('SELECT * FROM post ORDER BY created_at DESC LIMIT 12');
+
+$page = $_GET['page'] ?? 1;
+if (!filter_var($page, FILTER_VALIDATE_INT)) {
+    throw new Exception('Numéro de page invalide');
+}
+
+if ($page === '1'){
+    header('Location: ' . $router->url('home'));
+    http_response_code(301);
+    exit();
+}
+$currentPage = (int)$page;
+if ($currentPage <= 0) {
+    throw new Exception('Numéro de page invalide');
+}
+
+$count = (int)$pdo->query('SELECT COUNT(id) FROM post')->fetch()[0];
+$perPage = 12;
+$pages = ceil($count / $perPage);
+if ($currentPage > $pages) {
+    throw new Exception('Cette page n\'existe pas');
+}
+$offset = $perPage * ($currentPage - 1);
+$query = $pdo->query("SELECT * FROM post ORDER BY created_at DESC LIMIT $perPage OFFSET $offset");
 $posts = $query->fetchAll(PDO::FETCH_CLASS, Post::class);
 
 ?>
@@ -19,4 +42,17 @@ $posts = $query->fetchAll(PDO::FETCH_CLASS, Post::class);
         <?php require 'card.php' ?>
     </div>
     <?php endforeach ?>
+</div>
+
+<div class="d-flex justify-content-between my-4">
+    <?php if ($currentPage > 1): ?>
+        <?php
+        $link = $router->url('home');
+        if ($currentPage > 2) $link .= '?page=' . ($currentPage - 1);
+        ?>
+        <a href="<?= $link ?>" class="btn btn-primary">Page précédente</a>
+    <?php endif ?>
+    <?php if ($currentPage < $pages): ?>
+        <a href="<?= $router->url('home') ?>?page=<?= $currentPage + 1 ?>" class="btn btn-primary ml-auto">Page suivante</a>
+    <?php endif ?>
 </div>
