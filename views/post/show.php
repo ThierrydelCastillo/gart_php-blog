140118064be5eeac695a1affede06829a376c7e4
ThierrydelCastillo <dev@del-castillo.fr>
2023-07-20 13:48:20 +0200

    07 - Page article

---
+++ views/post/show.php
@@ -0,0 +1,49 @@
+<?php
+
+use App\Connection;
+use App\Model\{Category, Post};
+
+$id = (int)$params['id'];
+$slug = $params['slug'];
+
+$pdo = Connection::getPDO();
+$query = $pdo->prepare("SELECT * from post WHERE id = :id");
+$query->execute(['id' => $id]);
+$query->setFetchMode(PDO::FETCH_CLASS, Post::class);
+/** @var Post|false */
+$post = $query->fetch();
+
+if ($post === false) {
+    throw new Exception('Aucun article ne correspond à cet ID');
+}
+
+if ($post->getSlug() !== $slug) {
+    $url = $router->url('post', ['slug' => $post->getSlug(), 'id' => $id]);
+    http_response_code(301);
+    header('Location: ' . $url);
+    exit();
+}
+
+$query = $pdo->prepare("
+    SELECT c.id, c.slug, c.name 
+    FROM post_category pc 
+    JOIN category c ON pc.category_id = c.id 
+    WHERE pc.post_id = :id
+");
+$query->execute(['id' => $id]);
+$query->setFetchMode(PDO::FETCH_CLASS, Category::class);
+/** @var Category[] */
+$categories = $query->fetchAll();
+
+?>
+
+<h1><?= e($post->getName()) ?></h1>
+<p class="text-muted"><?= $post->getCreatedAt()->format('d F Y') ?></p>
+<?php foreach($categories as $k => $category):
+    if($k > 0):
+       echo ', '; 
+    endif;
+    $category_url = $router->url('category', ['slug' => $category->getSlug(), 'id' => $category->getId()]);
+    ?><a href="<?= $category_url ?>"><?= e($category->getName()) ?></a><?php 
+endforeach ?>
+<p><?= $post->getFormattedContent() ?></p>
